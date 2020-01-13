6415bc745c3ed3239945a404cb9110b68d72a6ad
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-01-13 00:38:45 -0600

    Update download.sh

--- GWAS/download.sh
+++ GWAS/download.sh
@@ -1,1 +1,3 @@
-
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/NakamuraM_23000144_GCST001685/hum0076_1stgwas_160916.csv
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/CordellHJ_26394269_GCST003129/cordell_2015_26394269_pbc_efo1001486_1_gwas.sumstats.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/JiSG_27992413_GCST004030/ipscsg2016.result.combined.full.with_header.txt
